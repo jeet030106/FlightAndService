166fc4d50fff71760bcd838eef86547dd823ce6f
Hawkjeet <jeetmulani2006@gmail.com>
2026-05-13 15:50:54 +0530

    Model and City Repo Setup

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 const express=require("express");
 const bodyParser=require("body-parser");
 
+const CityRepository=require("./repository/city-repository");
+
 const {PORT} = require("./config/serverConfig");
 
 const setUpAndStartServer=async()=>{
-    
     const app=express();
-
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true}));
 
-    
     app.listen(PORT,()=>{
         console.log(`Server is running on port ${PORT}`);
+        const cityRepository = new CityRepository();
+        cityRepository.createCity({name:"Delhi"});
     });
 }
 
