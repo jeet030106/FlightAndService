a880e9ffdb7b1f3753bbb8edf61a4433076b86af
Hawkjeet <jeetmulani2006@gmail.com>
2026-05-14 02:40:21 +0530

    City Routes Implemented

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -4,5 +4,8 @@ const router = express.Router();
 const CityController = require('../../controllers/city-controller');
 
 router.post('/city', CityController.create);
+router.delete('/city/:id', CityController.destroy);
+router.get('/city/:id', CityController.getCity);
+router.put('/city/:id', CityController.updateCity);
 
 module.exports = router;
