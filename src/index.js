188d409f969ddaeb23f6f16e4bd2286dd16e470d
Hawkjeet <jeetmulani2006@gmail.com>
2026-05-14 02:39:03 +0530

    City Routes Implemented

--- src/index.js
+++ src/index.js
@@ -2,18 +2,20 @@ const express=require("express");
 const bodyParser=require("body-parser");
 
 const CityRepository=require("./repository/city-repository");
+const routes=require("./routes/index");
 
 const {PORT} = require("./config/serverConfig");
 
 const setUpAndStartServer=async()=>{
+
     const app=express();
+
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true}));
 
+    app.use('/api', routes);
     app.listen(PORT,()=>{
         console.log(`Server is running on port ${PORT}`);
-        const cityRepository = new CityRepository();
-        cityRepository.createCity({name:"Delhi"});
     });
 }
 
