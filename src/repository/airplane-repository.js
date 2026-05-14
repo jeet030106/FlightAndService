98709b8abde30ce62d9c450e108121d4bda3f8cb
Hawkjeet <jeetmulani2006@gmail.com>
2026-05-15 02:07:46 +0530

    Repo for Airplane

---
+++ src/repository/airplane-repository.js
@@ -0,0 +1,14 @@
+const {Airplane} = require('../models/index');
+
+class AirplaneRepository{
+
+    async getAirplane(airplaneId){
+        try {
+            const airplane = await Airplane.findByPk(airplaneId);
+            return airplane;
+        } catch (error) {
+            console.log("Error in Repository Layer", error);
+            throw error;
+        }
+    }
+}
